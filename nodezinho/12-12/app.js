8a796032fdc598fd2802095e443cb9ac23177806
Cassiano <saurinho.64.ss@gmail.com>
2025-12-12 14:37:08 -0300

    Feat terminando a atividade do dia 12/12

--- nodezinho/12-12/app.js
+++ nodezinho/12-12/app.js
@@ -5,7 +5,11 @@ const cors = require("cors")
 app.use(express.urlencoded())
 app.use(cors());
 app.set("view engine", "ejs")
-app.get("/", (req, res) => res.send("Faz o L!"))
+app.get("/", (req, res) => res.render("index"))
+app.get("/ola", (req, res) => res.render("ola"))
+app.get("/enchendo-linguica", (req, res) => res.render("enchendolinguica"))
+
+app.use((req, res, next) => res.redirect("/"))
 
 
 app.listen(3000, () => console.log("http://localhost:3000"));
